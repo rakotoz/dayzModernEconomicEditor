66095090c430c59e48b9a9b1bde6a8a369146483
Alex Efremov <alexefremovoz@gmail.com>
2026-05-08 11:37:19 +0400

    Initial React app

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+
+root.render(<>Test</>);
--- src/renderer.ts
+++ src/renderer.ts
@@ -27,7 +27,6 @@
  */
 
 import './index.css';
+import './app';
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
-);
+console.log('👋 This message is being logged by "renderer.ts", included via Vite');
